6d3809863e6efb02dff831e9aff3d61cfc93a834
Isabella Basso <isabellabdoamaral@gmail.com>
2025-10-09 17:23:29 -0300

    try to build for windows

--- build.bat
+++ build.bat
@@ -1,1 +1,19 @@
-echo "See joltc/build/ and run one of the cmake builds. Open the .sln in Visual Studio and compile the projects. Copy resultant .dll to root of project"
+@echo off
+
+setlocal EnableDelayedExpansion
+
+if not exist joltc\JoltPhysics\NUL (
+    git clone --recurse-submodules https://github.com/jrouwe/JoltPhysics -b v5.3.0 --depth=1 joltc\JoltPhysics
+)
+
+set binaries_dir=build
+
+echo Configuring build...
+cmake . -B %binaries_dir% -DJPH_BUILD_SHARED=OFF
+
+echo Building project...
+make -C %binaries_dir%
+
+copy /y %binaries_dir%\lib\joltc.lib .\
+
+echo Build completed successfully!
