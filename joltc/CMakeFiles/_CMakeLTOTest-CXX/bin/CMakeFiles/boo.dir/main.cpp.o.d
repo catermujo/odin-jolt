d649323d22fd602bd84c6224a803d8b9756febcb
Isabella Basso <isabellabdoamaral@gmail.com>
2026-08-09 06:25:36 -0300

    repo: ignore generated CMake output

--- joltc/CMakeFiles/_CMakeLTOTest-CXX/bin/CMakeFiles/boo.dir/main.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/boo.dir/main.cpp.o: \
-  /Users/wolfie/studio/git/vendor/jolt/bindgen/joltc/CMakeFiles/_CMakeLTOTest-CXX/src/main.cpp
--- joltc/CMakeFiles/_CMakeLTOTest-CXX/bin/CMakeFiles/foo.dir/foo.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/foo.dir/foo.cpp.o: \
-  /Users/wolfie/studio/git/vendor/jolt/bindgen/joltc/CMakeFiles/_CMakeLTOTest-CXX/src/foo.cpp
